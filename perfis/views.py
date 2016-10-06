f8d402ddbe96f22843c1e4b96dca43827a0f808d
jmfcientista <jmfcientista@gmail.com>
2016-10-06 10:59:02 -0300

    segundo commit

--- perfis/views.py
+++ perfis/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
